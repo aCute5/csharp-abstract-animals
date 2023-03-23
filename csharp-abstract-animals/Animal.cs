9525ee3ccaeffbc35503df323a5deb7625c26176
Dario Costantini <costantini95dario@gmail.com>
2023-03-23 14:48:52 +0100

    Aggiungere i file di progetto.

---
+++ csharp-abstract-animals/Animal.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    internal class MondoAnimale
+    {
+        public abstract class Animale
+        {
+            public abstract void Verso();
+            public abstract void Eat();
+            public static void Sleep()
+            {
+                Console.WriteLine("ZZZZZ");
+            }
+        }
+
+
+        public class Cane : Animale
+        {
+            public override void Verso()
+            {
+                Console.WriteLine("Bau");
+            }
+            public override void Eat()
+            {
+                Console.WriteLine("Cane è un carnivoro");
+            }
+        }
+
+        public class Passerotto : Animale
+        {
+            public override void Verso()
+            {
+                Console.WriteLine("Piu Piu");
+            }
+            public override void Eat()
+            {
+                Console.WriteLine("Mangia quello che trova");
+            }
+        }
+
+        public class Aquila : Animale
+        {
+            public override void Verso()
+            {
+                Console.WriteLine("Boh");
+            }
+            public override void Eat()
+            {
+                Console.WriteLine("Conigli");
+            }
+        }
+        public class Delfino : Animale
+        {
+            public override void Verso()
+            {
+                Console.WriteLine("EEEEEh");
+            }
+            public override void Eat()
+            {
+                Console.WriteLine("Atrli pesci");
+            }
+         
+        }
+        public interface IVola
+        {
+            void Vola();
+        }
+        public interface INuota
+        {
+            void Nuota();
+        }
+
+        public class Uccello :  Animale,IVola  // pazzesco! posso concatenare le astrazioni
+        { 
+            public override void Verso()
+            {
+                Console.WriteLine("EEEEEh");
+            }
+            public override void Eat()
+            {
+                Console.WriteLine("Atrli pesci");
+            }
+            public void Vola()
+            {
+                Console.WriteLine("Sto Volando");
+            }
+       
+        }
+        public class Pesce : Animale,INuota
+        {
+            public override void Verso()
+            {
+                Console.WriteLine("non penso faccia versi");
+            }
+            public override void Eat()
+            {
+                Console.WriteLine("Altri pesci");
+            }
+            public void Nuota()
+            {
+                Console.WriteLine("Sto nuotando");
+            }
+        }
+
+    }
+}
---
+++ csharp-abstract-animals/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using static csharp_abstract_animals.MondoAnimale;
+
+// Instanziamento di classe figle della classe astratta animal
+Cane fido = new Cane();
+fido.Verso();
+fido.Eat();
+
+Delfino doggy = new Delfino();
+doggy.Verso();
+doggy.Eat();
+
+
+Uccello uccello = new Uccello();
+Pesce pesce = new Pesce();
+
+takeVolo(uccello); // questo metodo prende come parametro un instaziamento specifico di tipo uccello che ha come interfaccia necessria IVola e prende il metodo che caratterizza il volare di questo animale. 
+takeNuoto(pesce);
+static void takeVolo(IVola animal)
+{
+    animal.Vola();
+}
+static void takeNuoto(INuota animal)
+{
+    animal.Nuota();
+}
+
